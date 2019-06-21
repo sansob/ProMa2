9d699658215e228cdd00dee4376351821cdcd496
Lendon <hisyam.lateaf@gmail.com>
2019-06-21 18:10:30 +0700

    add Client/Controllers/RulesController.cs add Client/Views/Rules/Index.cshtml add Client/Views/Rules/Index.cshtml

---
+++ Client/Controllers/RulesController.cs
@@ -0,0 +1,46 @@
+﻿using Core.Base;
+using DataAccess.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class RulesController : Controller
+    {
+        BaseLink get = new BaseLink();
+
+        // GET: Rules
+        public ActionResult Index()
+        {
+            return View(LoadRule());
+        }
+
+        public JsonResult LoadRule()
+        {
+            IEnumerable<RuleVM> ruleVM = null;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(get.link)
+            };
+            var responseTask = client.GetAsync("Rules");
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<IList<RuleVM>>();
+                readTask.Wait();
+                ruleVM = readTask.Result;
+            }
+            else
+            {
+                ruleVM = Enumerable.Empty<RuleVM>();
+                ModelState.AddModelError(string.Empty, "Server error");
+            }
+            return Json(ruleVM, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
