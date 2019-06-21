909aa774dbbc0beda643878353ed9e6e10707539
Gian <gianpetriciaaurora@gmail.com>
2019-06-21 16:45:59 +0700

    Update Client/Controllers/RepliesController.cs Update Client/Controllers/TicketsController.cs Update Client/Controllers/RepliesController.cs Update Client/Controllers/TicketsController.cs Update Core/Base/BaseLink.cs

---
+++ Client/Controllers/RepliesController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    
+
+    public class RepliesController : Controller
+    {
+        BaseLink get = new BaseLink();
+
+        // GET: Replies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        
+    }
+}
---
+++ Client/Controllers/TicketsController.cs
@@ -0,0 +1,47 @@
+﻿using Core.Base;
+using DataAccess.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class TicketsController : Controller
+    {
+        BaseLink get = new BaseLink();
+        
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            return View(LoadTicket());
+        }
+
+        public JsonResult LoadTicket()
+        {
+            IEnumerable<TicketVM> ticketVM = null;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(get.link)
+            };
+            var responseTask = client.GetAsync("Tickets");
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<IList<TicketVM>>();
+                readTask.Wait();
+                ticketVM = readTask.Result;
+            }
+            else
+            {
+                ticketVM = Enumerable.Empty<TicketVM>();
+                ModelState.AddModelError(string.Empty, "Server error");
+            }
+            return Json(ticketVM, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
